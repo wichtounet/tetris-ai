54ba58c5fdf5f688b71337d583dcfe23699040e7
wichtounet <wichtounet@Desktop-PC>
2012-05-11 16:23:16 +0200

    Clean code

--- src/code/AppStart.java
+++ src/code/AppStart.java
@@ -1,61 +1,63 @@
 package code;
 
 import static code.ProjectConstants.formatStackTrace;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
-import java.awt.event.*;
+/*
+ * Class that starts the app!
+ */
+public class AppStart {
+    /*
+     * Errors go to console if true, otherwise go to GUI logger.
+     */
 
-import javax.swing.*;
+    public static final boolean REPORT_TO_CONSOLE = true;
 
-/*Class that starts the app!*/
-public class AppStart
-{
-	/*Errors go to console if true, otherwise go to GUI logger.*/
-	public static final boolean REPORT_TO_CONSOLE = true;
-	
-	public static void main(String... args)
-	{
-		System.setErr(System.out);
-		
-		try{
-			UIManager.setLookAndFeel(
-					UIManager.getSystemLookAndFeelClassName());
-		}catch(Throwable t){}
-		
-		//Better exception catching.
-		Thread.setDefaultUncaughtExceptionHandler(
-				new Thread.UncaughtExceptionHandler(){
+    public static void main(String... args) {
+        System.setErr(System.out);
 
-					public void uncaughtException(Thread t, Throwable e)
-					{
-						
-						if(REPORT_TO_CONSOLE)
-							e.printStackTrace();
-						else
-							JOptionPane.showMessageDialog(null,
-							"Java version: " + System.getProperty("java.version")
-							+ "\nOperating System: " + System.getProperty("os.name")
-							+ "\nFatal exception in thread: " + t.getName()
-							+ "\nException: " + e.getClass().getName()
-							+ "\nReason given: " + e.getMessage()
-							+ "\n\n"+formatStackTrace(e.getStackTrace()));
-						
-						System.exit(1);
-					}
-				});
-		
-		SwingUtilities.invokeLater(new Runnable(){
-			public void run()
-			{
-				//Get the ball rolling!
-				
-				new Thread(new Runnable()
-				{
-					public void run()
-					{
-						GameWindow win = new GameWindow();
-					}
-				}).start();
-			}
-		});
-	}
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (Throwable t) {
+        }
+
+        //Better exception catching.
+        Thread.setDefaultUncaughtExceptionHandler(
+                new Thread.UncaughtExceptionHandler() {
+
+                    public void uncaughtException(Thread t, Throwable e) {
+
+                        if (REPORT_TO_CONSOLE) {
+                            e.printStackTrace();
+                        } else {
+                            JOptionPane.showMessageDialog(null,
+                                    "Java version: " + System.getProperty("java.version")
+                                    + "\nOperating System: " + System.getProperty("os.name")
+                                    + "\nFatal exception in thread: " + t.getName()
+                                    + "\nException: " + e.getClass().getName()
+                                    + "\nReason given: " + e.getMessage()
+                                    + "\n\n" + formatStackTrace(e.getStackTrace()));
+                        }
+
+                        System.exit(1);
+                    }
+                });
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+                //Get the ball rolling!
+
+                new Thread(new Runnable() {
+
+                    public void run() {
+                        GameWindow win = new GameWindow();
+                    }
+                }).start();
+            }
+        });
+    }
 }
--- src/code/GameWindow.java
+++ src/code/GameWindow.java
@@ -1,139 +1,130 @@
 package code;
 
-import static code.ProjectConstants.*;
+import static code.ProjectConstants.STARTFS;
+import java.awt.Dimension;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-import java.awt.*;
-import java.io.IOException;
-import java.util.Arrays;
+/*
+ * The game window.
+ */
+public class GameWindow extends JFrame {
 
-import javax.sound.midi.*;
-import javax.swing.*;
+    private GraphicsDevice dev;
+    private TetrisPanel t;
 
-import code.SoundManager.Sounds;
+    /*
+     * Creates a GameWindow, by default.
+     */
+    public GameWindow() {
+        this(STARTFS, null);
+    }
 
+    /*
+     * Creates a GameWindow and make it fullscreen or not. May be from another GameWindow.
+     */
+    public GameWindow(boolean fullscreen, GameWindow old) {
+        super();
+        if (fullscreen) {
+            createFullscreenWindow(old);
+        } else {
+            createWindow(old);
+        }
 
-/*The game window.*/
-public class GameWindow extends JFrame
-{
-	
-	private GraphicsDevice dev;
-	private TetrisPanel t;
-	
-	
-	/*Creates a GameWindow, by default.*/
-	public GameWindow()
-	{
-		this(STARTFS, null);
-	}
-	
-	
-	/*Creates a GameWindow and make it fullscreen or not.
-	 * May be from another GameWindow.*/
-	public GameWindow(boolean fullscreen, GameWindow old)
-	{
-		super();
-		if(fullscreen)
-		{
-			createFullscreenWindow(old);
-		}
-		
-		else createWindow(old);
-		
-		if(old!=null)
-		{
-			//Cleanup
-			old.setVisible(false);
-			old.dispose();
-			old = null;
-		}
-	}
-	
-	
-	
-	private void createWindow(GameWindow old)
-	{
-		setUndecorated(false);
-		setTitle("JTetris");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(false);
-		
-		if(old != null)
-			t = old.t;
-		else t = new TetrisPanel();
-		
-		t.setPreferredSize(new Dimension(800,600));
-		setContentPane(t);
-		
-		pack();
-		setLocationRelativeTo(null);
-		setVisible(true);
-		
-		if(old==null)
-			t.engine.startengine();
-	}
-	
-	
-	
-	private void createFullscreenWindow(GameWindow old)
-	{
-		setUndecorated(true);
-		setTitle("JTetris");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(800, 600);
-		
-		if(old != null)
-			t = old.t;
-		else t = new TetrisPanel();
-		
-		t.setPreferredSize(new Dimension(800,600));
-		setContentPane(t);
-		
-		try{
-			dev =  GraphicsEnvironment
-				.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-			dev.setFullScreenWindow(this);
-			dev.setDisplayMode(new DisplayMode
-					(800,600,32,DisplayMode.REFRESH_RATE_UNKNOWN));
-		}catch(Throwable t){
-				throw new RuntimeException("Getting screen device failed");
-		}
-		
-		t.setPreferredSize(new Dimension(800,600));
-		setContentPane(t);
-		
-		setVisible(true);
-		SwingUtilities.updateComponentTreeUI(this);
-		
-		if(old==null)
-			t.engine.startengine();
-	}
-	
-	
-	/*Creates a fullscreen window from an old window.*/
-	public static GameWindow enterFullScreen(GameWindow win)
-	{
-		win = new GameWindow(true, win);
-		try{
-			win.dev.setFullScreenWindow(win);
-			//800x600 fullscreen?
-			win.dev.setDisplayMode(new DisplayMode
-					(800,600,32,DisplayMode.REFRESH_RATE_UNKNOWN));
-		
-		}catch(Throwable t)
-		{
-			win.dev.setFullScreenWindow(null);
-			throw new RuntimeException("Failed fullscreen");
-		}
-		return win;
-	}
-	
-	
-	/*Creates a windowed window (lol?) from an old window.*/
-	public static GameWindow exitFullScreen(GameWindow win)
-	{
-		if(win.dev != null)
-			win.dev.setFullScreenWindow(null);
-		win = new GameWindow(false, win);
-		return win;
-	}
+        if (old != null) {
+            //Cleanup
+            old.setVisible(false);
+            old.dispose();
+            old = null;
+        }
+    }
+
+    private void createWindow(GameWindow old) {
+        setUndecorated(false);
+        setTitle("JTetris");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        if (old != null) {
+            t = old.t;
+        } else {
+            t = new TetrisPanel();
+        }
+
+        t.setPreferredSize(new Dimension(800, 600));
+        setContentPane(t);
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        if (old == null) {
+            t.engine.startengine();
+        }
+    }
+
+    private void createFullscreenWindow(GameWindow old) {
+        setUndecorated(true);
+        setTitle("JTetris");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+
+        if (old != null) {
+            t = old.t;
+        } else {
+            t = new TetrisPanel();
+        }
+
+        t.setPreferredSize(new Dimension(800, 600));
+        setContentPane(t);
+
+        try {
+            dev = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+            dev.setFullScreenWindow(this);
+            dev.setDisplayMode(new DisplayMode(800, 600, 32, DisplayMode.REFRESH_RATE_UNKNOWN));
+        } catch (Throwable t) {
+            throw new RuntimeException("Getting screen device failed");
+        }
+
+        t.setPreferredSize(new Dimension(800, 600));
+        setContentPane(t);
+
+        setVisible(true);
+        SwingUtilities.updateComponentTreeUI(this);
+
+        if (old == null) {
+            t.engine.startengine();
+        }
+    }
+
+    /*
+     * Creates a fullscreen window from an old window.
+     */
+    public static GameWindow enterFullScreen(GameWindow win) {
+        win = new GameWindow(true, win);
+        try {
+            win.dev.setFullScreenWindow(win);
+            //800x600 fullscreen?
+            win.dev.setDisplayMode(new DisplayMode(800, 600, 32, DisplayMode.REFRESH_RATE_UNKNOWN));
+
+        } catch (Throwable t) {
+            win.dev.setFullScreenWindow(null);
+            throw new RuntimeException("Failed fullscreen");
+        }
+        return win;
+    }
+
+    /*
+     * Creates a windowed window (lol?) from an old window.
+     */
+    public static GameWindow exitFullScreen(GameWindow win) {
+        if (win.dev != null) {
+            win.dev.setFullScreenWindow(null);
+        }
+        win = new GameWindow(false, win);
+        return win;
+    }
 }
